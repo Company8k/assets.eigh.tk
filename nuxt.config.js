b7ccc0b656f11fb689d2d5612427d5ba12e0d40b
NT <thent87@eigh.tk>
2017-07-11 14:50:19 +0000

    sexy jazzzy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,18 @@
+/*eslint-env node*/
 module.exports = {
-  
-}
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: '' }
+    ],
+    bodyAttrs:{
+      class: 'eightk'
+    }
+  },
+  css: [
+    '~assets/style.css'
+  ]
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <h1>Welcome!</h1>
-    <nuxt-link to="/about">About page</nuxt-link>
   </div>
 </template>
+
+<script>
+  export default {
+    asyncData (context) {
+      // called every time before loading the component
+      return { name: 'World' }
+    },
+    fetch () {
+      // The fetch method is used to fill the store before rendering the page
+    },
+    head () {
+      return{
+        title: 'Index'
+      }
+    }
+  }
+</script>
