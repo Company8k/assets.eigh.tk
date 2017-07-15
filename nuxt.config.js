be384d5689bdf0b5561a2b3cfca03fe2306c4180
NT <thent87@eigh.tk>
2017-07-15 10:38:44 +0000

    schema.org

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,27 @@ module.exports = {
         rel: 'stylesheet', href: '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
         integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN', crossorigin: 'anonymous'
       }
-    ]
+    ],
+    script: [
+      { innerHTML: `{
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "url": "http://eigh.tk", "name": "Eightk",
+        "logo": "https://coinprism.blob.core.windows.net/profile/icon/AZHHLvzKbsEPZUQXvCfihkEknF71GHNpPH.jpg",
+        "brand": {
+          "@type": "Brand",
+          "name": "8K"
+        },
+        "department": {
+          "@type": "Organization",
+          "name": "8K Assets",
+          "email": "coin@eigh.tk",
+          "url": "https://assets.eigh.tk"
+        }
+      }`,
+        type: 'application/ld+json' }
+    ],
+    __dangerouslyDisableSanitizers: ['script']
   },
   css: [
     { src:'~assets/eightk.scss', lang:'scss' }
